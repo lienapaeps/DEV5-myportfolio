f2d4bd2316516ceb99ffea96d58970c7da208825
lienapaeps <paepsliena@gmail.com>
2022-10-05 12:03:59 +0200

    feat: weather api

---
+++ dev5-lab3/main.js
@@ -0,0 +1,4 @@
+import './style.css'
+import Weather from './src/Weather.js'
+
+const weather = new Weather("ba91f222bfee45829a070328220510");
---
+++ dev5-lab3/src/Weather.js
@@ -0,0 +1,61 @@
+export default class Weather {
+    constructor(api_key) {
+        this.apiKey = api_key;
+        // console.log("Weather class initialized " + this.apiKey);
+
+        // check if weather data is in localstorage and if timestamp older than 10 minutes
+        if (localStorage.getItem('weather') && Date.now() - localStorage.getItem('weather_timestamp') < 600000) {
+            // get data from localstorage
+            const weatherData = JSON.parse(localStorage.getItem('weather'));
+            this.displayWeather(weatherData);
+        } else {
+            this.getLocation();
+        }
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.getWeather.bind(this));
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }      
+    }
+
+    getWeather(position) {
+        console.log(this);
+        console.log(position);
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        const url = `https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${lat},${lon}&aqi=no`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data);
+
+                // save to localstorage
+                localStorage.setItem('weather', JSON.stringify(data));
+                // save timestamp
+                localStorage.setItem('weather_timestamp', Date.now());
+
+                this.displayWeather(data);
+            });
+    }
+
+    displayWeather(data) {
+        const temp = data.current.temp_c;
+        const weather = data.current.condition.text;
+        const location = data.location.name;
+
+        const img = document.createElement("img");
+        img.src = data.current.condition.icon;
+        img.alt = weather;
+
+        document.querySelector('.weather__temp').innerText = temp + "°C";
+        document.querySelector('.weather__summary').innerText = weather;
+        document.querySelector('.weather__location').innerText = location;
+        document.querySelector('.weather__icon').appendChild(img);
+    }
+
+}
